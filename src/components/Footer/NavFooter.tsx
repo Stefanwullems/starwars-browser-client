e5303c89a2b9180cf3fd6c4e9dab8bbd4a994ed9
Stefanwullems <stefanwullems1998@gmail.com>
2018-12-09 16:50:43 +0100

    footer navigation

--- src/components/Footer/NavFooter.tsx
+++ src/components/Footer/NavFooter.tsx
@@ -7,6 +7,9 @@ import {
   Button
 } from "@material-ui/core";
 import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import cleanUpContent from "../../redux/actions/clean-up-content";
 
 const styles = createStyles({
   footerContainer: {
@@ -19,30 +22,65 @@ const styles = createStyles({
   }
 });
 
-interface IProps extends WithStyles<typeof styles> {}
+interface IProps extends WithStyles<typeof styles> {
+  cleanUpContent: () => void;
+}
 
-const NavFooter: React.FC<IProps & RouteComponentProps> = props => {
-  const { classes } = props;
+class NavFooter extends React.Component<IProps & RouteComponentProps> {
+  getPageUrl = (path: string, id: number, page: "Prev" | "Next") => {
+    const n = page === "Prev" ? -1 : 1;
+    return path.replace(":id", (id + n).toString());
+  };
+  buttonStyle = (orientation: "Left" | "Right") => ({
+    marginRight: orientation === "Left" ? "20%" : "60%",
+    marginLeft: orientation === "Left" ? "60%" : "20%"
+  });
 
-  return (
-    <Grid container className={classes.footerContainer}>
-      <Grid item xs={6}>
-        <Button className={classes.button} style={buttonStyle("Left")}>
-          Prev
-        </Button>
+  handleClick() {
+    this.props.cleanUpContent();
+    this.forceUpdate();
+  }
+
+  render() {
+    const { classes } = this.props;
+    const path = this.props.match.path;
+    const id = Number(this.props.match.params["id"]);
+
+    return (
+      <Grid container className={classes.footerContainer}>
+        <Grid item xs={6}>
+          <Link
+            to={this.getPageUrl(path, id, "Prev")}
+            onClick={this.handleClick.bind(this)}
+          >
+            <Button className={classes.button} style={this.buttonStyle("Left")}>
+              Prev
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link
+            to={this.getPageUrl(path, id, "Next")}
+            onClick={this.handleClick.bind(this)}
+          >
+            <Button
+              className={classes.button}
+              style={this.buttonStyle("Right")}
+            >
+              Next
+            </Button>
+          </Link>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <Button className={classes.button} style={buttonStyle("Right")}>
-          Next
-        </Button>
-      </Grid>
-    </Grid>
-  );
-};
+    );
+  }
+}
 
-const buttonStyle = (orientation: "Left" | "Right") => ({
-  marginRight: orientation === "Left" ? "20%" : "60%",
-  marginLeft: orientation === "Left" ? "60%" : "20%"
-});
+const mdtp = { cleanUpContent };
 
-export default withStyles(styles)(NavFooter);
+const ConnectedNavFooter = connect(
+  null,
+  mdtp
+)(NavFooter);
+
+export default withStyles(styles)(ConnectedNavFooter);
--- src/components/Planets/PlanetsContainer.tsx
+++ src/components/Planets/PlanetsContainer.tsx
@@ -4,16 +4,22 @@ import fetchModel from "../../redux/actions/fetch-model";
 import cleanUpContent from "../../redux/actions/clean-up-content";
 import { RouteComponentProps } from "react-router";
 
-interface MappedProps extends RouteComponentProps {
+interface IProps {
   fetchModel: (model: Models, id: number) => void;
   cleanUpContent: () => void;
+  content: Content;
+  loading: boolean;
 }
 
-interface IProps extends MappedProps {}
-
-class PlanetsContainer extends React.Component<IProps> {
+class PlanetsContainer extends React.Component<IProps & RouteComponentProps> {
   componentDidMount() {
     this.props.fetchModel("planets", this.props.match.params["id"]);
+  }
+
+  async componentDidUpdate() {
+    if (!this.props.content && !this.props.loading) {
+      this.props.fetchModel("planets", this.props.match.params["id"]);
+    }
   }
 
   componentWillUnmount() {
@@ -25,7 +31,7 @@ class PlanetsContainer extends React.Component<IProps> {
   }
 }
 
-const mstp = ({ content }) => ({ content });
+const mstp = ({ content, loading }) => ({ content, loading });
 const mdtp = { fetchModel, cleanUpContent };
 
 export default connect(
